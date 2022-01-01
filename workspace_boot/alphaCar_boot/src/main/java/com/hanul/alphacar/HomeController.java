f574f1a56550013bf71a17882a0848eecf61c97b
holic4570 <mvsod@naver.com>
2022-01-01 22:12:42 +0900

    new

---
+++ workspace_boot/alphaCar_boot/src/main/java/com/hanul/alphacar/HomeController.java
@@ -0,0 +1,17 @@
+package com.hanul.alphacar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+			
+		return "index";
+	}
+	
+	
+}
---
+++ workspace_boot/alphaCar_boot/src/test/java/com/alphacar/AlphaCarBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaCarBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
